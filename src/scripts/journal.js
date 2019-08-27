c4d1fc38e5beb7b01441d4ea2f3b629525fd1275
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-08-27 15:35:28 -0500

    added journal entry function

---
+++ src/scripts/journal.js
@@ -0,0 +1,26 @@
+
+console.log("Hey");
+const journalEntry = []
+
+journalEntry.push({
+    dateOfEntry: "August 19th",
+    conceptsCovered: "no class-lab day",
+    moodOfTheDay: "euphoric",
+    entry: "worked on the daily journal page with css. I also added css to the govtrack page.",
+})
+
+journalEntry.push({
+    dateOfEntry: "August 20th",
+    conceptsCovered: "C33 Demo Day",
+    moodOfTheDay: "peaceful",
+    entry: "We watched C33 Demos. That afternoon we were split into group and started on group projects.",
+})
+
+journalEntry.push({
+    dateOfEntry: "August 21st",
+    conceptsCovered: "Group Project",
+    moodOfTheDay: "Pumped",
+    entry: "We worked on our group project and decided how to layout our webpage. We picked an artist that we wanted to make a tribute page for.",
+})
+
+console.log("journalEntry", journalEntry);
