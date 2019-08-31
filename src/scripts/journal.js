6252a9717264c7ca79e17f998cf88a1f550d289f
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-08-31 14:33:19 -0500

    part3

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,26 +1,60 @@
 
 console.log("Hey");
-const journalEntry = []
+const journalEntries = []
 
-journalEntry.push({
+journalEntries.push({
     dateOfEntry: "August 19th",
     conceptsCovered: "no class-lab day",
     moodOfTheDay: "euphoric",
     entry: "worked on the daily journal page with css. I also added css to the govtrack page.",
 })
 
-journalEntry.push({
+journalEntries.push({
     dateOfEntry: "August 20th",
     conceptsCovered: "C33 Demo Day",
     moodOfTheDay: "peaceful",
     entry: "We watched C33 Demos. That afternoon we were split into group and started on group projects.",
 })
 
-journalEntry.push({
+journalEntries.push({
     dateOfEntry: "August 21st",
     conceptsCovered: "Group Project",
     moodOfTheDay: "Pumped",
     entry: "We worked on our group project and decided how to layout our webpage. We picked an artist that we wanted to make a tribute page for.",
 })
 
-console.log("journalEntry", journalEntry);
+console.log("journalEntries", journalEntries);
+
+
+const makeJournalEntryComponent = (entry) =>{
+    // Create your own HTML structure for a journal entry
+    return `
+    <div>
+        <h2>${entry.dateOfEntry}</h2>
+        <p>${entry.conceptsCovered}</p>
+        <p>${entry.moodOfTheDay}</p>
+        <p>${entry.entry}</p>
+    </div>
+    `
+}
+
+const entryLog = document.querySelector(".entryLog");
+
+
+
+const renderJournalEntries = (entries) =>{
+    for (let i = 0; i < journalEntries.length; i++){
+        console.log("loop of the array", i , journalEntries[i]);
+        entryLog.innerHTML += makeJournalEntryComponent(journalEntries[i]);  
+    }
+}    
+
+renderJournalEntries(journalEntries);
+// example of Array Iteration
+// var txt = "";
+// var numbers = [45, 4, 9, 16, 25];
+// numbers.forEach(myFunction);
+
+// function myFunction(value, index, array) {
+//   txt = txt + value + "<br>"; 
+// }
